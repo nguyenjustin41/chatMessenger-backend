0f6c2c3a9be06af6e4bce32053e1aac0bc9d27d9
nguyenjustin41 <53131589+nguyenjustin41@users.noreply.github.com>
2022-01-13 23:00:25 -0800

    third commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors())
 const io = new Server(server, { 
     cors: {
         origin: "http://localhost:3000",
-        // methods: ["GET", "POST"],
+        methods: ["GET", "POST"],
     }
 });
 
